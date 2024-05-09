d26af72aa7bdc6702d98e2ec8a4e36c41b3b0c5a
navnath13 <navnathdoke2222@gmail.com>
2024-05-08 20:50:43 -0700

    this is the first commit

---
+++ src/main/java/com/example/firstprojectwithasw/FirstprojectwithaswApplication.java
@@ -0,0 +1,20 @@
+package com.example.firstprojectwithasw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstprojectwithaswApplication {
+
+	@GetMapping("/")
+	 public String home() {
+		 return "good job your project is added successfully in aws ";
+	 }
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectwithaswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstprojectwithasw/FirstprojectwithaswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstprojectwithasw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectwithaswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
