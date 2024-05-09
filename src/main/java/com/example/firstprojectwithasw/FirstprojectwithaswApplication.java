17716814155f2d733e4eca03acbc51c326be52c5
navnath13 <navnathdoke2222@gmail.com>
2024-05-09 03:41:52 -0700

    siind

--- src/main/java/com/example/firstprojectwithasw/FirstprojectwithaswApplication.java
+++ src/main/java/com/example/firstprojectwithasw/FirstprojectwithaswApplication.java
@@ -11,7 +11,7 @@ public class FirstprojectwithaswApplication {
 
 	@GetMapping("/")
 	 public String home() {
-		 return "good job your project is added successfully";
+		 return "good job your project is added successfullys";
 	 }
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectwithaswApplication.class, args);
